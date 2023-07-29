c47b1a3c882b3d23988e6fd12f257ec5238b8bf0
Nhật Mưn Phạm <minhmeomommeo@gmail.com>
2023-07-29 12:31:13 +0700

    update 29-7

--- react-form/src/components/Form.js
+++ react-form/src/components/Form.js
@@ -3,9 +3,12 @@ import img1 from '../assets/img1.jpg';
 import { useRef, useState, useEffect } from 'react';
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from '../api/axios';
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const EMAIL_REGEX = /^ [A-Za-z0-9+_.-]+@ (.+)$/;
+const REGISTER_URL = '/register'
 
 export default function Form() {
     const userRef = useRef();
@@ -43,116 +46,155 @@ export default function Form() {
         setErrMsg('');
     }, [user, pwd, matchPwd])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(
+                REGISTER_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    header: { 'Content-type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(response.data);
+            console.log(response.accessToken);
+            console.log(response.stringify(response));
+            setSuccess(true);
+        } catch (error) {
+            if (!error.response) {
+                setErrMsg('No Server Response');
+            } else if (error.response?.status === 409) {
+                errMsg('Username Taken');
+            } else {
+                errMsg('Registration Failed')
+            }
+            errRef.current.focus();
+        }
+    }
+
     return (
-        <section>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live='assertive'>{errMsg}</p>
-            <div className="register">
-                <div className="col-1">
-                    <h1>Sign Up</h1>
-                    <span>Register and enjoy!</span>
-
-                    {/* FORM */}
-                    <form id="form" className='flex flex-col'>
-
-                        {/* USERNAME IPNUT */}
-                        <label htmlFor='username'>
-                            Username:
-                            <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
-                            <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
-                        </label>
-                        <input
-                            type="text" placeholder='username'
-                            id='username'
-                            ref={userRef}
-                            autoComplete='off'
-                            onChange={(e) => setUser(e.target.value)}
-                            required
-                            aria-invalid={validName ? "false" : "true"}
-                            aria-describedby='uidnote'
-                            onFocus={() => setUserFocus(true)}
-                            onBlur={() => setUserFocus(false)}
-                        >
-                        </input>
-                        <p id='uidnote' className={userFocus && user && !validName ? "instructions" : "offscreen"}>
-                            <FontAwesomeIcon icon={faInfoCircle} />
-                            4 to 24 characters. <br />
-                            Must begin with a letter. <br />
-                            Letters, numbers, underscores, hyphens allowed.
-                        </p>
-
-                        {/* PASSWORD IPNUT */}
-                        <label htmlFor='pwd'>
-                            Password:
-                            <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
-                            <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
-                        </label>
-                        <input
-                            type="password" placeholder='password'
-                            id='pwd'
-                            onChange={(e) => setPwd(e.target.value)}
-                            required
-                            aria-invalid={validPwd ? "false" : "true"}
-                            aria-describedby='pwdnote'
-                            onFocus={() => setPwdFocus(true)}
-                            onBlur={() => setPwdFocus(false)}
-                        />
-                        <p id='pwdnote' className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
-                            <FontAwesomeIcon icon={faInfoCircle} />
-                            8 to 24 characters. <br />
-                            Must include uppercase and lowercase letters,
-                            a number and a special characters. <br />
-                            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
-                        </p>
-
-                        {/*CONFIRM PASSWORD IPNUT */}
-                        <label htmlFor="confirm_pwd">
-                            Confirm Password:
-                            <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
-                            <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
-                        </label>
-                        <input
-                            placeholder='confirm password'
-                            type="password"
-                            id="confirm_pwd"
-                            onChange={(e) => setMatchPwd(e.target.value)}
-                            value={matchPwd}
-                            required
-                            aria-invalid={validMatch ? "false" : "true"}
-                            aria-describedby="confirmnote"
-                            onFocus={() => setMatchFocus(true)}
-                            onBlur={() => setMatchFocus(false)}
-                        />
-                        <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
-                            <FontAwesomeIcon icon={faInfoCircle} />
-                            Must match the first password input field.
-                        </p>
-
-                        {/* EMAIL IPNUT */}
-                        <label htmlFor="email">
-                            Email:
-                        </label>
-                        <input
-                            type="text" placeholder='email'
-                            id="email"
-                        />
-
-                        {/* RESIGTER BUTTON */}
-                        <button className='btn' disabled={!validName || !validPwd || !validMatch ? true : false}>
-                            Register
-                        </button>
-                    </form>
+        <>
+            {success ? (
+                <section>
+                    <h1>Success!</h1>
                     <p>
-                        Already registered?<br />
-                        <span>
-                            <a href="#">Sign In</a>
-                        </span>
+                        <a href='#'>Sign In</a>
                     </p>
-                </div>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live='assertive'>{errMsg}</p>
+                    <div className="register">
+                        <div className="col-1">
+                            <h1>Sign Up</h1>
+                            <span>Register and enjoy!</span>
 
-                <div className="col-2">
-                    <img src={img1} alt="plant"></img>
-                </div>
-            </div>
-        </section>
+                            {/* FORM */}
+                            <form id="form" className='flex flex-col' onSubmit={handleSubmit}>
+
+                                {/* USERNAME IPNUT */}
+                                <label htmlFor='username'>
+                                    Username:
+                                    <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
+                                    <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
+                                </label>
+                                <input
+                                    type="text" placeholder='username'
+                                    id='username'
+                                    ref={userRef}
+                                    autoComplete='off'
+                                    onChange={(e) => setUser(e.target.value)}
+                                    required
+                                    aria-invalid={validName ? "false" : "true"}
+                                    aria-describedby='uidnote'
+                                    onFocus={() => setUserFocus(true)}
+                                    onBlur={() => setUserFocus(false)}
+                                >
+                                </input>
+                                <p id='uidnote' className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                                    <FontAwesomeIcon icon={faInfoCircle} />
+                                    4 to 24 characters. <br />
+                                    Must begin with a letter. <br />
+                                    Letters, numbers, underscores, hyphens allowed.
+                                </p>
+
+                                {/* PASSWORD IPNUT */}
+                                <label htmlFor='pwd'>
+                                    Password:
+                                    <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
+                                    <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
+                                </label>
+                                <input
+                                    type="password" placeholder='password'
+                                    id='pwd'
+                                    onChange={(e) => setPwd(e.target.value)}
+                                    required
+                                    aria-invalid={validPwd ? "false" : "true"}
+                                    aria-describedby='pwdnote'
+                                    onFocus={() => setPwdFocus(true)}
+                                    onBlur={() => setPwdFocus(false)}
+                                />
+                                <p id='pwdnote' className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                                    <FontAwesomeIcon icon={faInfoCircle} />
+                                    8 to 24 characters. <br />
+                                    Must include uppercase and lowercase letters,
+                                    a number and a special characters. <br />
+                                    Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                                </p>
+
+                                {/*CONFIRM PASSWORD IPNUT */}
+                                <label htmlFor="confirm_pwd">
+                                    Confirm Password:
+                                    <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
+                                    <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
+                                </label>
+                                <input
+                                    placeholder='confirm password'
+                                    type="password"
+                                    id="confirm_pwd"
+                                    onChange={(e) => setMatchPwd(e.target.value)}
+                                    value={matchPwd}
+                                    required
+                                    aria-invalid={validMatch ? "false" : "true"}
+                                    aria-describedby="confirmnote"
+                                    onFocus={() => setMatchFocus(true)}
+                                    onBlur={() => setMatchFocus(false)}
+                                />
+                                <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                                    <FontAwesomeIcon icon={faInfoCircle} />
+                                    Must match the first password input field.
+                                </p>
+
+                                {/* EMAIL IPNUT */}
+                                <label htmlFor="email">
+                                    Email:
+                                </label>
+                                <input
+                                    type="text" placeholder='email'
+                                    id="email"
+                                />
+
+                                {/* RESIGTER BUTTON */}
+                                <button className='btn' disabled={!validName || !validPwd || !validMatch ? true : false}>
+                                    Register
+                                </button>
+                            </form>
+                            <p>
+                                Already registered?<br />
+                                <span>
+                                    <a href="#">Sign In</a>
+                                </span>
+                            </p>
+                        </div>
+
+                        <div className="col-2">
+                            <img src={img1} alt="plant"></img>
+                        </div>
+                    </div>
+                </section>
+            )}
+        </>
     )
 }
